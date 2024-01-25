63c312779591774935386188f2f8ea9423c3d208
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2024-01-26 04:54:04 +0500

    Seminar 05 and Homework (v1)

---
+++ src/taskhw11/taskhw11.java
@@ -0,0 +1,87 @@
+// В обычный калькулятор без логирования добавьте возможность отменить
+// последнюю операцию.
+//
+// Отмена последней операции должна быть реализована следующим образом:
+// если передан оператор '<' калькулятор должен вывести результат предпоследней операции.
+//
+//        calculate('+', 3, 7)
+//        calculate('+', 4, 7)
+//        calculate('<', 0, 0)
+//
+//// 10.0
+//// 11.0
+//// 10.0
+//
+//        calculate('*', 3, 2)
+//        calculate('-', 7, 4)
+//        calculate('<', 0, 0)
+//
+//// 6.0
+//// 3.0
+//// 6.0
+
+
+package taskhw11;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Calculator {
+    Deque <Double> stack = new ArrayDeque<>();
+    public double calculate(char op, int a, int b) {
+        // Напишите свое решение ниже
+        switch (op) {
+            case '+':
+                stack.push((double) a + b);
+                return (double) a + b;
+            case '-':
+                stack.push((double) a - b);
+                return (double) a - b;
+            case '*':
+                stack.push((double) a * b);
+                return (double) a * b;
+            case '/':
+                stack.push((double) a / b);
+                return (double) a / b;
+            default:
+
+                return stack.pollLast();
+        }
+    }
+}
+
+
+// Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
+public class taskhw11 {
+    public static void main(String[] args) {
+        int a, b, c, d;
+        char op, op2, undo;
+
+        if (args.length == 0) {
+            // При отправке кода на Выполнение, вы можете варьировать эти параметры
+            a = 3;
+            op = '+';
+            b = 7;
+            c = 4;
+            op2 = '+';
+            d = 7;
+            undo = '<';
+        } else {
+            a = Integer.parseInt(args[0]);
+            op = args[1].charAt(0);
+            b = Integer.parseInt(args[2]);
+            c = Integer.parseInt(args[3]);
+            op2 = args[4].charAt(0);
+            d = Integer.parseInt(args[5]);
+            undo = args[6].charAt(0);
+        }
+
+        Calculator calculator = new Calculator();
+        double result = calculator.calculate(op, a, b);
+        System.out.println(result);
+        double result2 = calculator.calculate(op2, c, d);
+        System.out.println(result2);
+        double prevResult = calculator.calculate(undo, 0, 0);
+        System.out.println(prevResult);
+    }
+}
